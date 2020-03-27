1f581025ef64db5089a0ebd31a37679804f15e86
augustovds7 <augustovds7@gmailcom>
2020-03-27 16:54:25 -0400

    Atualizações

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var op = 1;
-var player1 = player1;
-var player2 = player2;
+var player1 = 0;
+var player2 = 0;
 
 function clique_aqui(e, id){
 	
@@ -170,4 +170,33 @@ function reset_fields(){
 
 		elemento.innerHTML = ""
 	}	
+}
+
+function invert_fields(){
+
+	var marca_1 = document.getElementById("marca1");
+	var marca2 = document.getElementById("marca2");
+	var aux = marca_1.innerHTML;
+
+	marca_1.innerHTML = marca2.innerHTML;
+
+
+	marca2.innerHTML = aux;
+}
+
+function point_add(){
+
+	var marca = document.getElementById("marca1");
+	var p1 = document.getElementById("p1_wins");
+
+	if (marca.innerHTML == "X"){
+	
+
+		p1.innerHTML = "1";
+	}
+
+	//console.log(p1.p1_wins.innerHTML);
+
+	p1.p1_wins =  1;
+
 }
